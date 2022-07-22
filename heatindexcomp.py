1209c050ffe79c463c276b0fc9250859bd3a7344
Emma Hauser <emma.hauser@umt.edu>
2022-07-22 10:59:59 -0600

    reading the printing module

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,6 +1,6 @@
 
 from readdata import read_data
-
+from printing import print_comparison 
 
 ###Column names and column indices
 columns = {'date': 0, 'time': 1, 'tempout': 2, 'humout': 5, 'heatindex': 13}
@@ -37,15 +37,6 @@ for temp, hum  in zip(data['tempout'], data['humout']):
  
 
 ##Output comparison of data
-print('                      ORIGINAL    ESTIMATED')
-print('   DATE      TIME    HEATINDEX    HEATINDEX    DIFFERENCE')
-print('-------    ------    ---------    ---------    ----------')
-zip_data = zip(data['date'], data['time'], data['heatindex'], heatindex)
-for date, time, hi_data, hi_est in zip_data:
-    hi_diff = hi_data - hi_est
-    print(f'{date}    {time:>6}    {hi_data:9.6f}    {hi_est:9.6f}    {hi_diff:10.6f}')
-
-#for wc_data, wc_est in zip(data['windchill'], windchill):
-    #print(f'{wc_data:.5f}   {wc_est:.5f}    {wc_data - wc_est:.5f}')
+print_comparison("HEATINDEX", data['date'], data['time'], data['heatindex'], heatindex)
 
 
--- windchillcomp.py
+++ windchillcomp.py
@@ -1,6 +1,6 @@
 
 from readdata import read_data
-
+from printing import print_comparison 
 
 ###Column names and column indices
 columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7, 'windchill': 12}
@@ -23,16 +23,7 @@ for temp, windspeed in zip(data['tempout'], data['windspeed']):
  
 
 ##Output comparison of data
-print('                      ORIGINAL    ESTIMATED')
-print('   DATE      TIME    WINDCHILL    WINDCHILL    DIFFERENCE')
-print('-------    ------    ---------    ---------    ----------')
-zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
-for date, time, wc_data, wc_est in zip_data:
-    wc_diff = wc_data - wc_est
-    print(f'{date}    {time:>6}    {wc_data:9.6f}    {wc_est:9.6f}    {wc_diff:10.6f}')
-
-#for wc_data, wc_est in zip(data['windchill'], windchill):
-    #print(f'{wc_data:.5f}   {wc_est:.5f}    {wc_data - wc_est:.5f}')
+print_comparison('WINDCHILL', data['date'], data['time'], data['windchill'], windchill)
 
 
 ##Debug
